436a6fd7e38a8405d7e041aaf553cf51b58dc050
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-03-23 23:18:57 +0200

    Difference between instance and static members.mp4

--- 01.java development SE/classProject/MainClass.java
+++ 01.java development SE/classProject/MainClass.java
@@ -2,5 +2,10 @@ public class MainClass {
     public static void main(String[] args) {
         SerratedDisc myCarDisc = new SerratedDisc(5, 3);
         myCarDisc.spin();
+        SerratedDisc.setCOMPNYNAME("namecompny");
+        String x = SerratedDisc.getCOMPNYNAME();
+        System.out.println(x);
+
     }
+
 }
--- 01.java development SE/classProject/SerratedDisc.java
+++ 01.java development SE/classProject/SerratedDisc.java
@@ -1,6 +1,13 @@
 public class SerratedDisc{
     private int size;
     private int numberOfPins;
+    private static String COMPNYNAME;
+    public static void setCOMPNYNAME(String name){
+        COMPNYNAME = name;
+    }
+    public static String getCOMPNYNAME(){
+        return COMPNYNAME;
+    }
     public SerratedDisc(int size, int numberOfPins){
        this.size = size;
        this.numberOfPins = numberOfPins;

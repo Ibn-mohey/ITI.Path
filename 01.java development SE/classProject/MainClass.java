9665845515bfd9470f174555260ebf16211cedfc
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-03-23 22:53:09 +0200

    Understanding constructor concepts to define objects.mp4

---
+++ 01.java development SE/classProject/MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+    public static void main(String[] args) {
+        SerratedDisc myCarDisc = new SerratedDisc(5, 3);
+        myCarDisc.spin();
+    }
+}
--- 01.java development SE/classProject/SerratedDisc.java
+++ 01.java development SE/classProject/SerratedDisc.java
@@ -1,6 +1,11 @@
 public class SerratedDisc{
     private int size;
     private int numberOfPins;
+    public SerratedDisc(int size, int numberOfPins){
+       this.size = size;
+       this.numberOfPins = numberOfPins;
+
+    }
     public void spin(){
         System.out.println("The Disc is spining");
     }

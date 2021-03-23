ebb6a6d74d683510b2263b2e15d0ba11a3139925
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-03-24 01:54:27 +0200

    abstract classes

---
+++ 01.java development SE/classProject/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+    abstract public void play();
+    
+}
+public class Dog extends Animal {
+
+    public void play(){
+    System.out.println("woof")
+    }    
+}
--- 01.java development SE/classProject/SerratedDisc.java
+++ 01.java development SE/classProject/SerratedDisc.java
@@ -1,19 +1,39 @@
+import java.util.Set;
+
 public class SerratedDisc{
     private int size;
     private int numberOfPins;
     private static String COMPNYNAME;
+
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    public void setNumberOfPins(int numberOfPins){
+        this.numberOfPins = numberOfPins;
+    }
+
+    public int getSize(){
+        return size;
+    }
+    public int getNumberOfPins(){
+        return numberOfPins;
+    }
+
     public static void setCOMPNYNAME(String name){
         COMPNYNAME = name;
     }
     public static String getCOMPNYNAME(){
         return COMPNYNAME;
     }
+
     public SerratedDisc(int size, int numberOfPins){
        this.size = size;
        this.numberOfPins = numberOfPins;
 
     }
     public void spin(){
+
         System.out.println("The Disc is spining");
     }
 }

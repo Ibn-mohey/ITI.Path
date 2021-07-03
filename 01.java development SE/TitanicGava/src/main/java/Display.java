21e4c3acf2dbc8949ee713b0182e95536f728139
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-07-03 04:56:40 +0200

    CSV files read
     load the Titanic data set provided as a csv file
    
     Using joinery and Tablesaw perform some data exploration activities such as
    getting information about the data (min, max, standard deviation, etc.)

--- 01.java development SE/TitanicGava/src/main/java/Display.java
+++ 01.java development SE/TitanicGava/src/main/java/Display.java
@@ -1,5 +1,7 @@
+import joinery.DataFrame;
+import tech.tablesaw.api.Table;
+
 import java.io.IOException;
-import java.util.List;
 
 public class Display {
     public static void main(String args[]) throws IOException {
@@ -10,14 +12,25 @@ public class Display {
 //        df = df.retain("bodyContentLength", "titleLength", "numberOfHeaders");
 //        DataFrame<Object> describe = df.describe();
 //        System.out.println(describe.toString());
+        /*
         List<Passenger> passengerList = new ReadFileJson().readPassengersJsonFile();
 //        System.out.println(passengersDataset);
+
         XChartApplications Charts = new XChartApplications();
         Charts.graphPassengerAges(passengerList);
         Charts.graphPassengerClass(passengerList);
         Charts.graphPassengerSurvived(passengerList);
         Charts.graphPassengerSurvivedGender(passengerList);
+*/
 
+        DataFrame df = new ReadFileCSV().readPassengersCSVFile1();
+        System.out.println(df.retain("age", "sibsp", "parch", "fare").max());
+        System.out.println(df.retain("age", "sibsp", "parch", "fare").min());
+        System.out.println("that is odd to have 0 fare if it was python, r or excel i would have checked using it for these value and how time it occurred");
+        System.out.println(df.retain("age", "sibsp", "parch", "fare").stddev());
+        System.out.println(df.retain("age", "sibsp", "parch", "fare").sum());
+        Table table = new ReadFileCSV().readPassengersCSVFile2();
+        System.out.println(table.summary());
 
     }
 

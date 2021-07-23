bb959645482a7fbbcf91a24d3b4d525be2b6debf
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-07-23 12:26:55 +0200

    small rename

--- 01.java development SE/Final project/src/main/java/com/example/ABS/ITI/FirstProject/WuzzufJobsAnalysis.java
+++ 01.java development SE/Final project/src/main/java/com/example/ABS/ITI/FirstProject/WuzzufJobsAnalysis.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalJavaProjectIttApplication {
+public class WuzzufJobsAnalysis {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalJavaProjectIttApplication.class, args);
+		SpringApplication.run(WuzzufJobsAnalysis.class, args);
 	}
 
 }

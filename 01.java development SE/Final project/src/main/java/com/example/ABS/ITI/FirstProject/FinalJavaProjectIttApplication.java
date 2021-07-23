77c755267e9baced4b20e6015a266fa0b9833ef7
Ibn-mohey <abdelrhman.mohey96@gmail.com>
2021-07-23 10:47:10 +0200

    Create the spring part
    this spring part was added  to be our web service backend
    initial idea from [here](https://www.youtube.com/watch?v=YDRNMAJo0MA&t=523s)
    made with [this tool](https://start.spring.io/)
    on jdk 8 with maven and spring boot 2.53
    added only spring web Dependencies

---
+++ 01.java development SE/Final project/src/main/java/com/example/ABS/ITI/FirstProject/FinalJavaProjectIttApplication.java
@@ -0,0 +1,13 @@
+package com.example.ABS.ITI.FirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalJavaProjectIttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalJavaProjectIttApplication.class, args);
+	}
+
+}
